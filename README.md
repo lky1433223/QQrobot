6b2033e31186bcf6596ab7b8e7ed1cea4d6e0d74
ShiinaRikka <541665621@qq.com>
2022-02-10 23:02:41 +0800

    1.0.0
    1.0.0开发基本完成

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# MC-QQrobot
-基于MCDR和go-cqhttp机器人进行QQ群和MC服务器互联
+# QQ robot
+
+一个 [MCDReforged](https://github.com/Fallen-Breath/MCDReforged)(>=2.X) 插件
+
+使用go-cqhttp实现在服务器里向QQ群发送消息的功能。
+
+
+# 功能介绍
+自主处理用户消息并发送群消息
+
+接收群消息（正在开发）
+
+在服务器启动、启动完成、关闭时发送消息
+
+在玩家进入/退出时发送消息
+
+在MCDR关闭时发送消息
+
+# 配置
+**需要配置好[go-cqhttp](https://docs.go-cqhttp.org/)** 连接方式选择http
+
+配置文件：```config/qq_robot/config.json```
+
+```ip```：go-cqhttp的http端口，默认值：```http://127.0.0.1:5700```
+
+```group_id```：发送消息的群号，默认值：```0```
+
+# Command
+```!!qq```显示帮助信息
+
+```!!qq send <message>``` 发送消息
---
+++ qq_robot/__init__.py
@@ -0,0 +1,110 @@
+# from email import message_from_bytes
+from email import message
+import time
+from tokenize import group
+import urllib.request
+import urllib.response
+import urllib.parse
+from flask import Flask, request
+from mcdreforged.api.all import *
+from typing import Dict, Callable, Any, Optional, Union, List
+# from numpy import Inf
+# https://docs.go-cqhttp.org/
+
+
+class Config(Serializable):
+    information: Dict[str, str] = {
+        'ip': 'http://127.0.0.1:5700',
+        'group_id': '0'
+    }
+
+
+config: Config
+
+
+def send_message(id='/send_group_msg', msg=None):
+    """
+    send message
+    :param id : str
+    :param msg : str
+    :return _UrlopenRet
+    """
+    global config
+    print(msg)
+    ip = config.information['ip']
+    group_id = int(config.information['group_id'])
+    url = ip + id
+    data = {'group_id': group_id, 'message': msg}
+    data = urllib.parse.urlencode(data).encode('utf8')
+    request = urllib.request.Request(url, data)
+    response = urllib.request.urlopen(request)
+    return response
+
+
+def on_load(server: PluginServerInterface, old):
+    server.logger.info('QQrobot on')
+    global config
+    config = server.load_config_simple('config.json', target_class=Config)
+
+    def exe(func: Callable[[CommandSource, str], Any]):
+        return lambda src, ctx: func(src, **ctx)
+    server.register_help_message(
+        '!!qq',
+        {
+            'en_us': 'qqrobot',
+            'zh_cn': 'QQ机器人'
+        }
+    )
+    server.register_command(
+        Literal('!!qq').
+        runs(lambda src: src.reply('!!qq send + message')).
+        then(
+            Literal('send').
+            then(
+                GreedyText('message').
+                runs(lambda src: src.reply('sending'))
+            )
+        )
+    )
+    # listen_qq(server)
+    # response = urllib.request.urlopen(
+    #     'http://192.168.3.13:5700/send_group_msg?group_id=1067245310&message=QQrobot_online')
+
+
+def on_user_info(server: PluginServerInterface, info: Info):
+    txt = info.content
+    if txt[0:9] == "!!qq send":
+        msg = str("player:") + str(info.player) + ('\n') + \
+            str("message:") + str(info.content[9:])
+        response = send_message('/send_group_msg', msg)
+        server.logger.info(response.read().decode('utf-8'))
+
+
+def on_server_start(server: PluginServerInterface):
+    response = send_message(msg='server starting')
+
+
+def on_server_startup(server: PluginServerInterface):
+    response = send_message(msg='server started')
+
+
+def on_server_stop(server: PluginServerInterface, server_return_code: int):
+    if(server_return_code != 0):
+        response = send_message(
+            msg='Server stopped unexpected, is it a server crash?')
+    else:
+        response = send_message(msg='Server stopped safely')
+
+
+def on_player_joined(server: PluginServerInterface, player: str, info: Info):
+    msg = player + ' joined the game'
+    response = send_message('/send_group_msg', msg)
+
+
+def on_player_left(server: PluginServerInterface, player: str):
+    msg = player + ' left the game'
+    response = send_message('/send_group_msg', msg)
+
+
+def on_mcdr_stop(server: PluginServerInterface):
+    response = send_message('/send_group_msg', 'MCDR stopped')
